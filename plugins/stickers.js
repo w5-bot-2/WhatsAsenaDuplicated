6c08718436d628a832bcd97f474d47634004aa7f
test-w5 <85579777+test-w5@users.noreply.github.com>
2021-06-19 01:33:16 +0530

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -64,7 +64,8 @@ else if (Config.WORKTYPE == 'public') {
                 .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
                 .save('st.webp')
                 .on('end', async () => {
-                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker ,{quoted: message.data}
+              );
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
@@ -75,7 +76,8 @@ else if (Config.WORKTYPE == 'public') {
             .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
             .save('sticker.webp')
             .on('end', async () => {
-                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
+                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker ,{quoted: message.data}
+              );
             });
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
