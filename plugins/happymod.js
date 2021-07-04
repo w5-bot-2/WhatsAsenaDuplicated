573a88915a8e3674b3b3b7a617b08604b0a31eab
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-07-04 12:45:00 +0530

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -34,12 +34,13 @@ Asena.addCommand({pattern: 'playstore ?(.*)', fromMe: false, desc: Lang.HMODD_DE
 		const response = await got(url);
 		const json = JSON.parse(response.body);
 		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
-		'*📕 ' + Lang.TITLE +'* ```' + json.result[0].title + '```\n\n' +
-		'*📗 ' + Lang.APP_ID +':* ```' + json.result[0].appid + '```\n\n' +
-		'*📗 ' + Lang.DEVELOPER +':* ```' + json.result[0].developer + '```\n\n' +									 
-		'*📗 ' + Lang.PRICE +':* ```' + json.result[0].url + '```\n\n' +									 
-		'*📘 ' + Lang.RATING +'* ```' + json.result[0].rating + '```\n\n\n' + 
-		'*📗 ' + Lang.THUMB +':* ```' + json.result[0].thumb + '```\n', MessageType.text);
+		'*🔰 ' + Lang.TITLE +'* ```' + json.result[0].title + '```\n\n' +
+		'*🔰 ' + Lang.APP_ID +':* ```' + json.result[0].appid + '```\n\n' +
+		'*🔰 ' + Lang.DEVELOPER +':* ```' + json.result[0].developer + '```\n\n' +									 
+		'*🔰 ' + Lang.PRICE +':* ```' + json.result[0].url + '```\n\n' +									 
+		'*🔰 ' + Lang.RATING +'* ```' + json.result[0].rating + '```\n\n\n' +
+		'*🔰 ' + Lang.URL +'* ```' + json.result[0].url + '```\n\n\n' +									 
+		'*🔰 ' + Lang.THUMB +':* ```' + json.result[0].thumb + '```\n', MessageType.text);
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 	}
