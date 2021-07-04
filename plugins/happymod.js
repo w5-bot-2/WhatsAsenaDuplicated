96bc465aad04c9ebe3048bb9678d635b189286ea
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-07-04 12:36:10 +0530

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -27,3 +27,20 @@ Asena.addCommand({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DES
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 	}
 });
+Asena.addCommand({pattern: 'playstore ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
+	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
+	const url = `https://api.zeks.xyz/api/sgplay?apikey=w6pOcZAlefcsPoNoFV8CzWpo9yT&q=${match[1]}`;
+	try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
+		'*📕 ' + Lang.TITLE +'* ```' + json.result[0].title + '```\n\n' +
+		'*📗 ' + Lang.APP_ID +':* ```' + json.result[0].appid + '```\n\n' +
+		'*📗 ' + Lang.DEVELOPER +':* ```' + json.result[0].developer + '```\n\n' +									 
+		'*📗 ' + Lang.PRICE +':* ```' + json.result[0].url + '```\n\n' +									 
+		'*📘 ' + Lang.RATING +'* ```' + json.result[0].rating + '```\n\n\n' + 
+		'*📗 ' + Lang.THUMB +':* ```' + json.result[0].thumb + '```\n', MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+	}
+});
