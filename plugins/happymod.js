0129b4a730bb474684b231da380df1229e8bd731
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-07-05 12:05:29 +0530

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -19,12 +19,12 @@ Asena.addCommand({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DES
 		const response = await got(url);
 		const json = JSON.parse(response.body);
 		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
-		'*🔰 ' + Lang.NAMEY +'* ```' + json.result[0].title + '```\n\n' +
-		'*🔰 ' + Lang.DOWNLOAD +':* ```' + json.result[0].url + '```\n\n' +									 
-		'*🔰 ' + Lang.RATING +'* ```' + json.result[0].rating + '```\n\n\n' + 
-		'*🔰 ' + Lang.THUMB +':* ```' + json.result[0].thumb + '```\n', MessageType.text);
+		'*🔰 ' + TITLE +':* ```' + json.result[0].title + '```\n\n' +
+		'*🔰 ' + URL +':* ```' + json.result[0].url + '```\n\n' +									 
+		'*🔰 ' + RATING +':* ```' + json.result[0].rating + '```\n\n\n' + 
+		'*🔰 ' + THUMBNAIL +':* ```' + json.result[0].thumb + '```\n', MessageType.text);
 	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text , {quoted: message.data});
 	}
 });
 Asena.addCommand({pattern: 'playstore ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
@@ -34,14 +34,14 @@ Asena.addCommand({pattern: 'playstore ?(.*)', fromMe: false, desc: Lang.HMODD_DE
 		const response = await got(url);
 		const json = JSON.parse(response.body);
 		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
-		'*🔰 ' + Lang.TITLE +'* ```' + json.result[0].title + '```\n\n' +
-		'*🔰 ' + Lang.APP_ID +':* ```' + json.result[0].appid + '```\n\n' +
-		'*🔰 ' + Lang.DEVELOPER +':* ```' + json.result[0].developer + '```\n\n' +									 
-		'*🔰 ' + Lang.PRICE +':* ```' + json.result[0].url + '```\n\n' +									 
-		'*🔰 ' + Lang.RATING +'* ```' + json.result[0].rating + '```\n\n\n' +
-		'*🔰 ' + Lang.URL +'* ```' + json.result[0].url + '```\n\n\n' +									 
-		'*🔰 ' + Lang.THUMB +':* ```' + json.result[0].thumb + '```\n', MessageType.text);
+		'*🔰 ' + TITLE +':* ```' + json.result[0].title + '```\n\n' +
+		'*🔰 ' + APP_ID +':* ```' + json.result[0].appid + '```\n\n' +
+		'*🔰 ' + DEVELOPER +':* ```' + json.result[0].developer + '```\n\n' +									 
+		'*🔰 ' + PRICE +':* ```' + json.result[0].url + '```\n\n' +									 
+		'*🔰 ' + RATING +':* ```' + json.result[0].rating + '```\n\n\n' +
+		'*🔰 ' + URL +':* ```' + json.result[0].url + '```\n\n\n' +									 
+		'*🔰 ' + THUMB +':* ```' + json.result[0].thumb + '```\n', MessageType.text);
 	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text , {quoted: message.data});
 	}
 });
